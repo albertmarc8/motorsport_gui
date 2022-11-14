e7d94b6d8d8d345881b4a3da8b6b909a28cfc9aa
albertmarc8 <albertmarc8@gmail.com>
2022-11-14 18:01:25 +0100

    Extracted input/output methods from main class to an external class

--- motorsport_plotter.py
+++ motorsport_plotter.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 from tkinter import *
-from tkinter import filedialog
 from tkinter.ttk import Treeview
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 from matplotlib.figure import Figure
+from utils.IO import *
 
 
 class MotorsportPlotter:
@@ -11,27 +11,28 @@ class MotorsportPlotter:
     pad_containers = 5
     single_plot = False
 
-    def __init__(self, root):
+    def __init__(self, r):
         self.figure = None
         self.chart = None
         self.previous_subplot_2 = None
         self.previous_subplot = None
         self.filename = None
         self.table = None
-        self.data = None
-        self.root = root
+        self.data = []
+        self.root = r
         self.root.title("UJI Motorsport Plotter")
         self.root.geometry('1600x900')
 
         # Window menu
         my_menu = Menu(self.root)
         self.root.config(menu=my_menu)
-        my_menu.add_command(label="Import", command=self.import_data)
+        my_menu.add_command(label="Import", command=lambda: import_data(self.data))
         my_menu.add_command(label="View", command=self.view_plot)  # TODO add command=method to parameters
-        my_menu.add_command(label="Export", command=self.export_data)  # TODO add command=method to parameters
+        my_menu.add_command(label="Export", command=lambda: export_data(self.data))  # TODO add command=method to parameters
         my_menu.add_command(label="Single/Multiple graphs", command=self.change_view)
         tables_menu = Menu(my_menu)
-        my_menu.add_cascade(menu=tables_menu, label="View tables") # TODO fix a line that appears and duplicates the dropdown
+        my_menu.add_cascade(menu=tables_menu,
+                            label="View tables")  # TODO fix a line that appears and duplicates the dropdown
         tables_menu.add_command(label="View airflow")  # TODO add command=method to parameters
         tables_menu.add_command(label="View engine block temperature")  # TODO add command=method to parameters
         tables_menu.add_command(label="View oil pressure")  # TODO add command=method to parameters
@@ -47,7 +48,7 @@ class MotorsportPlotter:
 
         control_container = LabelFrame(self.root, text="controls", padx=60, pady=30)
         # Import Button
-        import_btn = Button(control_container, text="Import data from .txt", command=self.import_from_txt)
+        import_btn = Button(control_container, text="Import data from .txt", command=import_from_txt)
         # import_btn.pack()
 
         # Plot Button
@@ -55,131 +56,28 @@ class MotorsportPlotter:
         # plot_btn.pack()
 
         # Export Button
-        plot_btn = Button(control_container, text="Export data to .csv", command=self.export_to_csv)
+        plot_btn = Button(control_container, text="Export data to .csv", command=export_to_csv)
         # plot_btn.pack()
         control_container.grid(column=1, row=1, sticky=NSEW, padx=self.pad_containers, pady=self.pad_containers)
 
         self.chart_label_frame = LabelFrame(self.root, text="chart")  # , padx=750, pady=300)
         Label(self.chart_label_frame, text="TELEMETRY CHART", font=("Arial", 25)).place(relx=.5, rely=.5,
-                                                                                   anchor=CENTER)
+                                                                                        anchor=CENTER)
         self.chart_label_frame.grid(column=0, row=0, columnspan=2, sticky=NSEW, padx=self.pad_containers,
-                               pady=self.pad_containers)
+                                    pady=self.pad_containers)
 
         self.table_label_frame = LabelFrame(self.root, text="table")
         Label(self.table_label_frame, text="TELEMETRY TABLE", font=("Arial", 25)).place(relx=.5, rely=.5,
-                                                                                   anchor=CENTER)
+                                                                                        anchor=CENTER)
         self.table_label_frame.grid(column=0, row=1, sticky=NSEW, padx=self.pad_containers, pady=self.pad_containers)
         # table_label_frame.place(x=40, y=720, width=1200, height=330)
 
         self.toolbar = None
         # self.plot_on_start()
 
-    def plot_on_start(self):
-        self.filename = "datos_task/coche_reduced.txt"
-        # 1. Read the file:
-        with open(self.filename, "r") as f:
-            # 2. Iterate and get values from all lines
-            self.data = [(int(seconds), int(rpms)) for seconds, rpms in
-                         [line[7:-8].split(", ") for line in f.readlines()]]
-        self.plot_data()
-
-    def import_from_txt(self):
-        """
-        Asks the user to select a txt file in which each line follows
-        the regex [0x001 seconds, rpms 0x001]
-        stores a resulting matix with shape (n, 2) being n the number
-        of non-empty lines in the file
-        """
-
-        self.filename = filedialog.askopenfilename(filetypes=(('text files', '*.txt'),))
-
-        # 1. Read the file:
-        with open(self.filename, "r") as f:
-            # 2. Iterate and get values from all lines
-            self.data = [(int(seconds), int(rpms)) for seconds, rpms in
-                         [line[7:-8].split(", ") for line in f.readlines()]]
-
-    def export_to_csv(self):
-        """
-        Saves 'self.data' into a file in the same folder as the txt
-        but with a csv filetype.
-        WARNING: It will remove the file with csv if it already exists
-        """
-
-        # 0. Check if a datafile has been loaded
-        if self.filename is None:
-            return None  # Todo: mensaje de error
-
-        # 1. Get the right path
-        filename = self.filename.rstrip("txt")
-        filename += "csv"
-
-        # 2. Open the file
-        with open(filename, "w") as f:
-            # 3. Strore data as csv
-            [f.write(line) for line in [",".join([str(d[0]), str(d[1])]) + "\n" for d in self.data]]
-
-    def import_data(self):
-        def read_data(file):
-            """
-            Opens a dialog and asks the user to select a file from which It can read the data from.
-
-            :param file: Reads the specified file line by line.
-            :return: Returns a 2d array with int/float/string variables from the file.
-            """
-            def line_convert(info):
-                """
-                Converts a line from the files to an array with correct data types.
-
-                :param info: The line that will be converted into an array.
-                :return: The properly formatted array containing ints, floats and strings.
-                """
-                variables = info.rstrip().split(",")
-
-                # Conversion: strings -> int
-                for num_i in {0, 2, 3, 8, 9, 11, 18, 19}:
-                    variables[num_i] = int(variables[num_i])
-
-                # Conversion: strings -> float
-                for num_f in {4, 5, 6, 7, 10, 12, 13, 14, 15, 16, 17}:
-                    variables[num_f] = float(variables[num_f])
-
-                # Taking into consideration the 2 kind of files, one with 20 fields and another with 22 (starting to count from 0)
-                if len(variables) == 21:
-                    for num_i in {20, 21}:
-                        variables[num_i] = int(variables[num_i])
-                return variables
-
-            data = []
-            for line in open(file).readlines():
-                data.append(line_convert(line))
-
-            return data
-
-        filename = filedialog.askopenfilename(defaultextension="txt")
-        self.data = read_data(filename)
-
-    def export_data(self):
-        """
-            Exports the data to a CSV using the ; as a separator (European standards).
-            File can be saved with any name and no file format is needed to be specified at the end.
-
-        :return: Method does not return anything.
-        """
-        if len(self.data) > 0:
-            filename = filedialog.asksaveasfile(mode="w", defaultextension=".csv")
-            sep = ";"  # delimitador CSV
-            intervalo = 1 #intervalo = int(espaciado_entry.get()) if espaciado_entry.get().isnumeric() else 1
-
-            for i in range(0, len(self.data), intervalo):
-                str2print = ""
-                for j in range(0, len(self.data[i])-1, intervalo):
-                    str2print += str(self.data[i][j]) + ";"
-                str2print += str(self.data[i][j]) + ";\n"
-                filename.write(str2print)
-            filename.close()
 
     def view_multi_plot(self):
+        # TODO some error appears uppon using this method
         # TODO fix RAM problems, if possible
         if self.data is None:
             return None
@@ -197,7 +95,7 @@ class MotorsportPlotter:
 
         [self.table.insert(parent="", index="end", iid=i, text="", values=d) for i, d in enumerate(self.data)]
 
-        #self.table.place(x=0, y=720, height=380, width=1280)
+        # self.table.place(x=0, y=720, height=380, width=1280)
         self.table.grid(column=0, row=1, sticky=NSEW, padx=self.pad_containers, pady=self.pad_containers)
         self.root.update()
 
@@ -211,8 +109,8 @@ class MotorsportPlotter:
             ax.set_title("Data plot")
             ax.plot(seconds, rpms)
 
-        #frame = Frame(self.root)
-        #frame.grid(column=0, row=0, columnspan=2, sticky=NSEW, padx=self.pad_containers, pady=self.pad_containers)
+        # frame = Frame(self.root)
+        # frame.grid(column=0, row=0, columnspan=2, sticky=NSEW, padx=self.pad_containers, pady=self.pad_containers)
 
         canvas = FigureCanvasTkAgg(fig, master=self.root)
         canvas.get_tk_widget().grid(column=0, row=0, columnspan=2, sticky=NSEW)
@@ -318,6 +216,7 @@ class MotorsportPlotter:
             self.view_single_plot()
         else:
             self.view_multi_plot()
+
 
 root = Tk()
 program = MotorsportPlotter(root)
