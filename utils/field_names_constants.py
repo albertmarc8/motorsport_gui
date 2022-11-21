d34de7f627cf24bd8a78c343dd7cd1b5e4fc2eb2
Albert Marc <albertmarc8@gmail.com>
2022-11-22 00:59:28 +0100

    For ease of use to get field names

---
+++ utils/field_names_constants.py
@@ -0,0 +1,23 @@
+Fields = ["Miliseconds",  # Field 0
+          "MIL",  # Field 1
+          "Num DTCs",  # Field 2
+          "Fuel system status",  # Field 3
+          "Engine load",  # Field 4
+          "Coolant temp",  # Field 5
+          "Air temp",  # Field 6
+          "Long term fuel trim",  # Field 7
+          "Intake manifold absolute pressure",  # Field 8
+          "Absolute barometric pressure",  # Field 9
+          "RPM",  # Field 10
+          "Vehicle speed",  # Field 11
+          "Timing advance",  # Field 12
+          "Throttle position",  # Field 13
+          "Relative throttle psoiton",  # Field 14
+          "Oxygen sensor voltage",  # Field 15
+          "Oxygen sensor long term fuel trim",  # Field 16
+          "Control module voltage",  # Field 17
+          "Distance Traveled MIL",  # Field 18
+          "//",  # Field 19
+          "String(GEAR::getGear()",  # Field 20
+          "OIL_PRESSURE_PIN"  # Field 21
+          ]
