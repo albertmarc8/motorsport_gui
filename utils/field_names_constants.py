6a0f356556ba121b36782f19533cdcdf051d9a21
albertmarc8 <albertmarc8@gmail.com>
2022-12-02 14:14:14 +0100

    Fix typo

--- utils/field_names_constants.py
+++ utils/field_names_constants.py
@@ -1,4 +1,4 @@
-Fields = ["Miliseconds",  # Field 0
+Fields = ["Milliseconds",  # Field 0
           "MIL",  # Field 1
           "Num DTCs",  # Field 2
           "Fuel system status",  # Field 3
@@ -12,7 +12,7 @@ Fields = ["Miliseconds",  # Field 0
           "Vehicle speed",  # Field 11
           "Timing advance",  # Field 12
           "Throttle position",  # Field 13
-          "Relative throttle psoiton",  # Field 14
+          "Relative throttle position",  # Field 14
           "Oxygen sensor voltage",  # Field 15
           "Oxygen sensor long term fuel trim",  # Field 16
           "Control module voltage",  # Field 17
