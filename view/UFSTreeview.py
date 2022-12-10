a377c1242c50ef6a1b50bd4ea58c4623c3449acc
Albert Marc <albertmarc8@gmail.com>
2022-12-10 16:19:33 +0100

    Fix implementation UFSTreeview

--- view/UFSTreeview.py
+++ view/UFSTreeview.py
@@ -7,8 +7,6 @@ from utils.field_names_constants import Fields
 class UFSTreeview(Treeview):
 
     def __init__(self, root):
-        super().__init__()
-
         style = Style()
         # Modify the font of the body
         style.configure("mystyle.Treeview", highlightthickness=0, bd=0, font=('Calibri', 11))
@@ -17,15 +15,16 @@ class UFSTreeview(Treeview):
         # Remove the borders
         style.layout("mystyle.Treeview", [('mystyle.Treeview.treearea', {'sticky': 'nswe'})])
 
-        self.table = Treeview(root, columns=([Fields[n] for n in range(len(Fields))]), style="mystyle.Treeview")
-        self.table.tag_configure('odd', background='#E8E8E8')
-        self.table.tag_configure('even', background='#DFDFDF')
+        super().__init__(root, columns=([Fields[n] for n in range(len(Fields))]), style="mystyle.Treeview")
 
-        self.table['show'] = 'headings'  # deletes the initial column without data (that is used for indexes/ids)
-        self.table['displaycolumns'] = ()
+        self.tag_configure('odd', background='#E8E8E8')
+        self.tag_configure('even', background='#DFDFDF')
+
+        self['show'] = 'headings'  # deletes the initial column without data (that is used for indexes/ids)
+        self['displaycolumns'] = ()
         self.table_vsb = Scrollbar(root, orient="vertical")
-        self.table.configure(yscrollcommand=self.table_vsb.set)
-        self.table_vsb.configure(command=self.table.yview)
+        self.configure(yscrollcommand=self.table_vsb.set)
+        self.table_vsb.configure(command=self.yview)
         for n in range(len(Fields)):
-            self.table.heading(n, text=Fields[n])
-        self.table.grid(column=0, row=1, sticky="NSEW", padx=(0, 20))
+            self.heading(n, text=Fields[n])
+        self.grid(column=0, row=1, sticky="NSEW", padx=(0, 20))
