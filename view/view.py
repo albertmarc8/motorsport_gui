0af6c777067910a7042664b56625356a0f56e08c
Ruubenf <rfemenia2001@gmail.com>
2022-12-12 10:47:12 +0100

    Checkpoint where table silhouette and the live data indicators are showing

--- view/view.py
+++ view/view.py
@@ -1,6 +1,9 @@
+from tkinter import LabelFrame, Label
+
 from customtkinter import CTk as Tk
 import customtkinter
 
+from view.UFSTreeview import UFSTreeview
 from view.menu import CustomMenu
 
 
@@ -23,6 +26,33 @@ class View(Tk):
         self.columnconfigure(1, weight=1)
         self.rowconfigure(0, weight=3)
         self.rowconfigure(1, weight=1)
+
+        # Table
+        self.table = UFSTreeview(self)
+
+        # Placeholders
+        self.control_container = LabelFrame(self, text="controls", pady=5)
+        self.control_container.columnconfigure(0, weight=1)
+        self.control_container.columnconfigure(1, weight=1)
+        self.control_container.rowconfigure(0, weight=1)
+        self.control_container.rowconfigure(1, weight=1)
+        self.control_container.rowconfigure(2, weight=1)
+        self.control_container.rowconfigure(3, weight=1)
+        self.control_container.grid(column=1, row=1, sticky="NSEW")
+
+        self.label_arduino_text = Label(self.control_container, text="Arduino Status:")
+        self.label_arduino_text.grid(column=0, row=0, sticky="NSEW")
+        self.label_arduino_status = Label(self.control_container, text="Not connected", fg='blue')
+        self.label_arduino_status.grid(column=1, row=0, sticky="NSEW")
+        self.label_time_text = Label(self.control_container, text="Moving seconds:")
+        self.label_time_text.grid(column=0, row=1, sticky="NSEW")
+        self.label_time_status = Label(self.control_container, text="ON", fg="green")
+        self.label_time_status.grid(column=1, row=1, sticky="NSEW")
+        #self.view_plot()
+
+
+
+
 
         #self.configure(background='light blue')
         #self.resizable(False, False)
