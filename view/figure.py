42bfa2f417db85658fe8eabeebf3dcde68d1fd81
Rubén Femenía Carrascosa <rfemenia2001@gmail.com>
2023-03-23 12:05:04 +0100

    Added legend to the chart

--- view/figure.py
+++ view/figure.py
@@ -4,6 +4,7 @@ from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 from customtkinter import CTkFrame as Frame
 from customtkinter import CTk as Tk
+#from utils.io import *
 
 
 class CustomFigure(Figure):
@@ -59,13 +60,14 @@ class CustomFigure(Figure):
             self.plotting_data.pop(title_index_in_plotting_data)
             self.ax.clear()
             for i, (x, y, title) in enumerate(self.plotting_data):
-                self.ax.plot(x, y)
+                self.ax.plot(x, y, label=title)
         else:
             self.plotting_data.append((x, y, title))
-            self.ax.plot(x, y)
+            self.ax.plot(x, y, label=title)
 
         #self.ax.plot(x, y)
         #self.ax.set_title(title, color=self.root.style.get_contrast_color())
+        self.ax.legend(loc="upper left")
         self.ax.ticklabel_format(useOffset=False, style='plain')
         self.canvas.draw()
 
@@ -79,3 +81,68 @@ class CustomFigure(Figure):
             if title == title:
                 return i
         return None
+
+
+    def plot_live_data(self, title):
+
+        counter = 0
+        y_data = [] 
+        x_data = []
+
+        self.ax.clear()
+        self.canvas.draw()
+        """
+        line = import_live_data()
+
+
+
+        if len(y_data[0]) < 100:
+            # Si hay menos de 100 datos para una magnitud
+            for sub_y in range(len(self.selected_ys)):
+                # y_data guarda los valores que se van a mostrar en pantalla
+                y_data[sub_y].append(line[self.selected_ys[sub_y]])
+
+
+
+
+
+
+
+        
+        if self.live_data_enabled:
+
+            line = import_live_data()
+            counter += 100
+
+            if len(y_data[0]) < 100:
+                # Si hay menos de 100 datos para una magnitud
+                for sub_y in range(len(self.selected_ys)):
+                    # y_data guarda los valores que se van a mostrar en pantalla
+                    y_data[sub_y].append(line[self.selected_ys[sub_y]])
+
+
+                x_data.append(line[self.selected_x[0]])
+                if line is not None:
+                    self.data.append(line)
+
+            else:
+                # Si ya hay 100 datos
+                for sub_y in range(len(self.selected_ys)):
+                    y_data[sub_y][0:99] = y_data[sub_y][1:100]
+                    y_data[sub_y][99] = line[self.selected_ys[sub_y]]
+
+                x_data[0:99] = x_data[1:100]
+                x_data[99] = line[self.selected_x[0]]
+                #x_data[99] = counter
+
+            for sub_y in range(len(self.selected_ys)):
+                self.lines[sub_y].set_xdata(x_data)
+                self.lines[sub_y].set_ydata(y_data[sub_y])
+
+            self.ax.set_ylim(0, max(y_data[0]) * 1.1)
+            self.ax.set_xlim(min(x_data), max(x_data))
+            self.canvas.draw()
+
+        if self.live_data_enabled:
+            self.root.after(10, self.plot_live_data)
+        """
